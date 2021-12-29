89c8ddb41f1a77838e6095a3982d82ee29d06ba8
shurasikk <90204027+shurasikk@users.noreply.github.com>
2021-12-29 20:31:11 +0300

    Add 1.2, 1.3

--- dm1/dm1/Program.cs
+++ dm1/dm1/Program.cs
@@ -18,7 +18,7 @@ public class CombObj
     {
         alphabet[i] = a;
     }
-    void Swap(int i, int j)
+    public void Swap(int i, int j)
     {
         int temp = obj[i];
         obj[i] = obj[j];
@@ -60,6 +60,88 @@ public class PermWithRep : CombObj
         return true;
     }
 }
+public class Permutations : CombObj
+{
+    public Permutations(int n, int k) : base(n, k)
+    {
+        this.n = n;
+        this.k = k;
+        obj = new int[n];
+        alphabet = new char[n];
+    }
+    public bool NextPerm()
+    {
+        int i = this.n - 2;
+        for (; i >= 0; i--)
+        {
+            if (obj[i] >= obj[i + 1]) continue;
+            else break;
+        }    
+        if (i == -1)
+        {
+            return false;
+        }
+        int j = this.n - 1;
+        for(; j>=0;j--)
+        {
+            if (obj[i] >= obj[j])
+                continue;
+            else break;
+        }
+        Swap(i, j);
+        int c = i + 1;
+        int r = this.n - 1;
+        while (c < r)
+        {
+            Swap(c++, r--);
+        }
+        return true;
+    }
+}
+
+public class PermNoRep : CombObj
+{
+    public PermNoRep(int n, int k): base(n,k)
+    {
+        this.n = n;
+        this.k = k;
+        obj = new int[n];
+        alphabet = new char[n];
+    }
+    public bool NextPNR()
+    {
+        int i;
+        do
+        {
+            i = this.n - 2;
+            for (; i >= 0; i--)
+            {
+                if (obj[i] >= obj[i + 1]) continue;
+                else break;
+            }
+            if (i == -1)
+            {
+                return false;
+            }
+            int j = this.n - 1;
+            for (; j >= 0; j--)
+            {
+                if (obj[i] >= obj[j])
+                    continue;
+                else break;
+            }
+            Swap(i, j);
+            int c = i + 1;
+            int r = this.n - 1;
+            while (c < r)
+            {
+                Swap(c++, r--);
+            }
+        }
+        while (i > this.k - 1);
+        return true;
+    }
+}
 
 namespace dm1
 {
@@ -72,18 +154,23 @@ namespace dm1
             Console.WriteLine("Введите k:");
             int k = Convert.ToInt32(Console.ReadLine());
             PermWithRep permwithrep_obj = new PermWithRep(n, k);
+            Permutations permut_obj = new Permutations(n, n);
+            PermNoRep pnr_obj = new PermNoRep(n, k);
             for(int i=0; i<n; i++)
             {
                 Console.WriteLine("Введите символ алфавита:");
                 char a = Convert.ToChar(Console.ReadLine());
                 permwithrep_obj.SetAlphabet(a,i);
+                permut_obj.SetAlphabet(a, i);
+                pnr_obj.SetAlphabet(a, i);
             }
+            //Построение размещений с повторениями по k элементов
             for(int i=0; i<k; i++)
             {
                 permwithrep_obj.obj[i] = 0;
             }
             StreamWriter countspwr = new StreamWriter(@"C:\Users\Asus\Documents\GitHub\dm3semester\dm1\dm1\task1.txt");
-            countspwr.WriteLine("Все перестановки с повторениями из {0} по {1} ", n,k);
+            countspwr.WriteLine("Все размещения с повторениями из {0} по {1} ", n,k);
             countspwr.Write("Алфавит: {");
             for (int i = 0; i < n; i++)
             {
@@ -104,6 +191,61 @@ namespace dm1
                 countspwr.Write(permwithrep_obj.alphabet[permwithrep_obj.obj[i]]);
             }
             countspwr.Close();
+            //Построение всех перестановок
+            StreamWriter countspermut = new StreamWriter(@"C:\Users\Asus\Documents\GitHub\dm3semester\dm1\dm1\task2.txt");
+            countspermut.WriteLine("Все перестановки с {0} элементами ", n);
+            countspermut.Write("Алфавит: {");
+            for (int i = 0; i < n; i++)
+            {
+                countspermut.Write(permut_obj.alphabet[i] + ", ");
+            }
+            countspermut.Write("}\n");
+            for (int i = 0; i < n; i++)
+            {
+                permut_obj.obj[i] = i;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                countspermut.Write(permut_obj.alphabet[permut_obj.obj[i]]);
+            }
+            countspermut.WriteLine();
+            while (permut_obj.NextPerm())
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    countspermut.Write(permut_obj.alphabet[permut_obj.obj[i]]);
+                }
+                countspermut.WriteLine();
+            }
+            countspermut.Close();
+            //Построение всех размещений по k
+            StreamWriter countsPNR = new StreamWriter(@"C:\Users\Asus\Documents\GitHub\dm3semester\dm1\dm1\task3.txt");
+            countsPNR.WriteLine("Все размещения без повторений с {0} элементами по {1}", n,k);
+            countsPNR.Write("Алфавит: {");
+            for (int i = 0; i < n; i++)
+            {
+                countsPNR.Write(pnr_obj.alphabet[i] + ", ");
+            }
+            countsPNR.Write("}\n");
+            for (int i = 0; i < n; i++)
+            {
+                pnr_obj.obj[i] = i;
+            }
+            for(int i=0; i<k; i++)
+            {
+                countsPNR.Write(pnr_obj.alphabet[pnr_obj.obj[i]]);
+            }
+            countsPNR.WriteLine();
+            while (pnr_obj.NextPNR()) 
+            {
+                for (int i = 0; i < k; i++)
+                {
+                    countsPNR.Write(pnr_obj.alphabet[pnr_obj.obj[i]]);
+                }
+                countsPNR.WriteLine();
+            }
+            countsPNR.Close();
+            //Построение всех подмножеств
         }
     }
 }
